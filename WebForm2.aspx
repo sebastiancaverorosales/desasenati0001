ff5a51b2e3ac69baedb3fc843431cfb0f58cf67b
sebastiancaverorosales <115047462+sebastiancaverorosales@users.noreply.github.com>
2023-03-11 11:54:32 -0500

    Add files via upload

---
+++ WebForm2.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            CALCULAR NOTAS<br />
+            INGRESAR CURSO<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            INGRESAR NOTA 1<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            INGRESAR NOTA 2<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            INGRESAR NOTA 3<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            INGRESAR NOTA 4<asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            <br />
+            PROMEDIO<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+            <br />
+            CONDICIÓN<asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="aceptar" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="limpiar" />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string curso = TextBox1.Text;
+            int nota = Convert.ToInt32(TextBox2.Text),
+                nota1 = Convert.ToInt32(TextBox3.Text)
+                , nota2 = Convert.ToInt32(TextBox4.Text)
+                , nota3 = Convert.ToInt32(TextBox5.Text);
+
+            int[] notas = { nota, nota1, nota2, nota3 };
+
+            int temporal = int.MaxValue;
+            float prom = 0;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (notas[i] < temporal)
+                {
+                    temporal = notas[i];
+                }
+            }
+
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (notas[i] != temporal)
+                {
+                    prom += notas[i];
+                }
+
+            }
+
+            prom = prom / 3;
+            TextBox6.Text = prom.ToString();
+            if (prom >= 10.5)
+            {
+                TextBox7.Text = "Aprobado";
+            }
+            else
+            {
+                TextBox7.Text = "Desaprobado";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+            TextBox4.Text = string.Empty;
+            TextBox5.Text = string.Empty;
+            TextBox6.Text = string.Empty;
+            TextBox7.Text = string.Empty;
+        }
+    }
+}
